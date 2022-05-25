7451c6b5e674fc51bd4e5a0afb4013d8489ab76b
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-05-25 08:36:50 -0400

    init

---
+++ data.js
@@ -0,0 +1,103 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stockpile",
+      "name": "Stockpile",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5664,
+      "initialViewParameters": {
+        "yaw": -0.08037867330471826,
+        "pitch": 0.3585155066270769,
+        "fov": 0.8269966563944086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1401371465255465,
+          "pitch": -0.017064238766655748,
+          "rotation": 0,
+          "target": "1-lower-end"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lower-end",
+      "name": "Lower End",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5568,
+      "initialViewParameters": {
+        "yaw": 0.050638913073838054,
+        "pitch": 0.2855634602677437,
+        "fov": 0.7533893979893007
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.516480130395724,
+          "pitch": 0.04764221145938485,
+          "rotation": 0,
+          "target": "0-stockpile"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Paw Paw 2 South 5-24-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
